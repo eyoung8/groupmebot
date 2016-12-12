e0fc6cbe8fea633cfd9a9862b4c7a69e8a6f2ecb
eyoung8 <eyoung8@binghamton.edu>
2016-12-12 05:03:23 -0500

    more logs

--- bot/util.py
+++ bot/util.py
@@ -1,4 +1,11 @@
 import requests
+import logging
+logger = logging.getLogger('testlogger')
+
 def send_response(bot_id, text):
+    logger.info("bot_id: " + bot_id)
+    logger.info("text: " + text)
     payload = {"bot_id": bot_id, "text": text}
+    logger.info(payload)
     r = requests.post('https://api.groupme.com/v3/bots/post', data=post_body)
+    logger.info("done")
