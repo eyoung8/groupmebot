f5553f7cc00709022ccbec042ba70abd0748df50
eyoung8 <eyoung8@binghamton.edu>
2016-12-12 03:14:17 -0500

    turned off debug

--- groupmebot/settings.py
+++ groupmebot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0ujwu2iue&m2!hcj@6-a2bdljy2fzngs9j)l0xq^pr)9vd&(rn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
