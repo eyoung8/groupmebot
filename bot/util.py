1184cb4a564973cb325d34a0050a4349a53e277d
eyoung8 <eyoung8@binghamton.edu>
2016-12-17 05:16:06 -0500

    reverting logger again :(

--- bot/util.py
+++ bot/util.py
@@ -2,26 +2,26 @@ import requests
 import logging
 from .models import Bot, BotResponse, MultipleResponse
 
+logger = logging.getLogger('testlogger')
 
 def log_enter_exit(func):
-    logger = logging.getLogger('testlogger')
     def wrapper():
         logger.info('entering')
         func()
         logger.info('exited')
     return wrapper
 
-@log_enter_exit
+#@log_enter_exit
 def send_response(bot_id, text):
     payload = {"bot_id": bot_id, "text": text}
     r = requests.post('https://api.groupme.com/v3/bots/post', data=payload)
 
-@log_enter_exit
+#@log_enter_exit
 def new_bot_response(bot, command, response):
     br = BotResponse(command=command, response=response, bot=bot)
     br.save()
 
-@log_enter_exit
+#@log_enter_exit
 def delete_bot_response(bot, command):
     try:
         bot_response = BotResponse.objects.get(command=command, bot=bot)
@@ -31,12 +31,12 @@ def delete_bot_response(bot, command):
         deleted = False
     return deleted
    
-@log_enter_exit
+#@log_enter_exit
 def new_multiple_response(bot, command, response):
     mr = MultipleResponse(command=command, response=response, bot=bot)
     mr.save()
 
-@log_enter_exit
+#@log_enter_exit
 def new_command(bot, text):
     response = ' '.join(text[1:])
     command = text[0]
@@ -46,7 +46,7 @@ def new_command(bot, text):
         new_bot_response(bot, command, response)
         send_response(bot.bot_id, "command {} successfully created".format(command))
 
-@log_enter_exit
+#@log_enter_exit
 def command(bot, command):
     try:
         bot_response = BotResponse.objects.get(bot=bot, command=command).response
@@ -54,14 +54,14 @@ def command(bot, command):
     except:
         pass
 
-@log_enter_exit
+#@log_enter_exit
 def new_random_command(bot, text):
     response = ' '.join(text[1:])
     command = text[0]
     new_multiple_response(bot, command, response)
     send_response(bot.bot_id, "random command {} successfully created".format(command))
 
-@log_enter_exit
+#@log_enter_exit
 def random_command(bot, text):
     command = text[0]
     try:
@@ -74,12 +74,12 @@ def random_command(bot, text):
     except:
         pass
 
-@log_enter_exit
+#@log_enter_exit
 def bot_help(bot, host):
     help_url ="".join(["https://", host, bot.get_absolute_url()])
     send_response(bot.bot_id, help_url)
 
-@log_enter_exit 
+#@log_enter_exit 
 def delete_command(bot, text):
     command = text[0]
     deleted = delete_bot_response(bot, command)
@@ -89,7 +89,7 @@ def delete_command(bot, text):
         msg = "Command not found or issue deleting command"
     send_response(bot.bot_id, msg)
 
-@log_enter_exit
+#@log_enter_exit
 def edit_command(bot, text):
     command = text[0]
     response = ' '.join(text[1:])
