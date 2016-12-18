2cf735b33264c2c5aeb58523e1724a9bc5b29ff2
eyoung8 <eyoung8@binghamton.edu>
2016-12-18 04:23:24 -0500

    changed settings for whitenoise

--- groupmebot/settings.py
+++ groupmebot/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
